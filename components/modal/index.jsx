b12b90fec04c229f7960ea9159e57c29d1f79989
yiminghe <yiminghe@gmail.com>
2015-06-05 12:06:54 +0800

    Updates

--- components/modal/index.jsx
+++ components/modal/index.jsx
@@ -21,18 +21,22 @@ module.exports = function (props) {
     if (props.onCancel) {
       props.onCancel();
     }
+    close();
+  }
+
+  function close() {
     d.setState({
       visible: false
     });
   }
 
   function onOk() {
-    if (props.onOk) {
-      props.onOk();
+    var onOk = props.onOk;
+    if (onOk) {
+      onOk(close);
+    } else {
+      close();
     }
-    d.setState({
-      visible: false
-    });
   }
 
   var footer = [
@@ -46,5 +50,8 @@ module.exports = function (props) {
   props.visible = true;
   props.children = props.content;
   props.footer = footer;
-  return React.render(<Dialog {...props}/>, div);
+  var d;
+  React.render(<Dialog {...props}/>, div, function () {
+    d = this;
+  });
 };
